ae81eb61159efb33264db996f7c9a5ca4a74b936
Keiichi Yamamoto <yamamoo@gmail.com>
2018-01-09 17:32:59 -0600

    Reduced to a single desktop

--- src/clock.sh
+++ src/clock.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 sleep 10
+
+# reduce the number of OpenBox desktops to 1 to avoid accidental desktop switching which can hide the emulator window
+wmctrl -n 1
+
+# append current time to the Appium Server terminal window title so that the recorded video always shows the time
 while true
 do
     wmctrl -r "Appium Server" -N "Appium Server # $(date '+%Y-%m-%d %H:%M:%S')"
