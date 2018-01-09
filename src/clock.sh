5f9c2c38677cbb98fff289175fbd978c99d4a32d
Keiichi Yamamoto <yamamoo@gmail.com>
2018-01-09 17:02:05 -0600

    Added missing -N parameter for wmctrl command

--- src/clock.sh
+++ src/clock.sh
@@ -2,6 +2,6 @@
 sleep 10
 while true
 do
-    wmctrl -r "Appium Server # $(date '+%Y-%m-%d %H:%M:%S')"
+    wmctrl -r "Appium Server" -N "Appium Server # $(date '+%Y-%m-%d %H:%M:%S')"
     sleep 1
 done
